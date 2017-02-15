fd597e9185acc7f12017f84af31e032aa813b245
Richard Wurth <Richard Wurth>
2017-02-14 19:58:30 -0700

    Initial and final submission of mini-userProfiles2

--- controller.js
+++ controller.js
@@ -1,7 +1,10 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	// $scope.users = mainService.getUsers();
+    mainService.getUsers().then(response => {
+      $scope.users = response.data.data;
+    });
   }
 
 
